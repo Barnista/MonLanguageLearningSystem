b121d9e67e13de3ada9fc5212a93baf0b116b4c7
Barnista <35296567+Barnista@users.noreply.github.com>
2025-06-18 01:32:33 +0700

    v0.3.0
    - added Compound Consonants Tables
    
    but not yet completed. it's only 1/2

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MonLanguageLearningSystem
 A web application to facilitate in learning the Mon language
+
+
+
+# Packages installed
+1. [simple-keyboard](https://www.npmjs.com/package/simple-keyboard)
+2. [meSpeak.js](https://www.masswerk.at/mespeak/)
+3. [phoneme-synthesis](https://github.com/itinerarium/phoneme-synthesis/)
+
+# References
+1. [Mon alphabet](https://en.wikipedia.org/wiki/Mon_alphabet)
+2. [อักษรมอญ](https://th.wikipedia.org/wiki/%E0%B8%AD%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B8%A3%E0%B8%A1%E0%B8%AD%E0%B8%8D)
+3. [Mon Anonta keyboard](https://keyman.com/keyboards/mon_anonta)
--- mon-language-learning-system/src/views/HomeView.vue
+++ mon-language-learning-system/src/views/HomeView.vue
@@ -3,6 +3,8 @@
     <CompConsonantTable class="mb-2"  />
     <hr>
     <CompVowelTable class="mt-5" />
+    <hr>
+    <CompCompoundCosonantTable class="mt-5" />
     <div class="my-5">
       <hr>
     </div>
@@ -11,6 +13,7 @@
 
 <script>
 // @ is an alias to /src
+import CompCompoundCosonantTable from '@/components/alphabets/CompCompoundCosonantTable.vue';
 import CompConsonantTable from '@/components/alphabets/CompConsonantTable.vue'
 import CompVowelTable from '@/components/alphabets/CompVowelTable.vue';
 
@@ -18,7 +21,8 @@ export default {
   name: 'HomeView',
   components: {
     CompConsonantTable,
-    CompVowelTable
+    CompVowelTable,
+    CompCompoundCosonantTable
   }
 }
 </script>
