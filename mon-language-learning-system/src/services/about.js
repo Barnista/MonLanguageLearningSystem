29698bc8f713fde13c5622a4975ed97ecc40f672
Barnista <35296567+Barnista@users.noreply.github.com>
2025-07-20 20:08:44 +0700

    for got to change version label

--- mon-language-learning-system/src/services/about.js
+++ mon-language-learning-system/src/services/about.js
@@ -2,5 +2,5 @@ export default {
     projectName: 'mon-language-learning-system',
     appName: 'The Mon Language (မန်)',
     appName2: 'The Mon Language: Learning System',
-    appVersion: '0.9.3'
+    appVersion: '`1.0.0'
 }
